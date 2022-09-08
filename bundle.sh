5f2bd0eb7a920d96c0f74cb01efe4fc82a784d1f
Varun Patil <radialapps@gmail.com>
2022-09-07 19:08:20 -0700

    Add bundle script

---
+++ bundle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+od=`pwd`
+
+make build-js-production
+
+rm -rf /tmp/memories
+cp -R . /tmp/memories
+
+cd /tmp
+rm -rf memories.tar.gz
+cd memories
+git clean -xdf
+rm -rf .git .gitignore Makefile package.json package-lock.json phpunit* webpack.js stylelint.config.js tests
+rm -rf src js/*.map appinfo/screencap.*
+rm -rf *.js .gitignore .npmignore
+cd ..
+
+tar -zvcf memories.tar.gz memories/
+openssl dgst -sha512 -sign ~/.nextcloud/certificates/memories.key memories.tar.gz | openssl base64
+rm -rf memories
+
+cd $od
+mv /tmp/memories.tar.gz .
