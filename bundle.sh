e2127010e869152f83435052a0d56f48fb8312eb
Varun Patil <radialapps@gmail.com>
2022-09-13 15:50:17 -0700

    Some clean up

--- bundle.sh
+++ bundle.sh
@@ -3,19 +3,13 @@
 od=`pwd`
 
 rm -rf /tmp/memories
-cp -R . /tmp/memories
+mkdir -p /tmp/memories
+cp -R appinfo css img js lib templates COPYING README.md exiftest* composer* /tmp/memories
 
 cd /tmp
 rm -rf memories.tar.gz
-cd memories
-git clean -xdf
-rm -rf .git .gitignore Makefile package.json package-lock.json phpunit* webpack.js stylelint.config.js tests
-rm -rf src js/*.map appinfo/screencap.*
-rm -rf *.js .gitignore .npmignore
-cd ..
 
 tar -zvcf memories.tar.gz memories/
-# openssl dgst -sha512 -sign ~/.nextcloud/certificates/memories.key memories.tar.gz | openssl base64
 rm -rf memories
 
 cd $od
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-$dt = \DateTime::createFromFormat('Y:m:d H:i:s', "1800:01:01 00:00:00");
-
-var_dump($dt->getTimestamp());
