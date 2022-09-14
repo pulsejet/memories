0db2d064651a5888840fe4ba2d2d4dab7c87b2f6
Varun Patil <radialapps@gmail.com>
2022-09-14 15:50:02 -0700

    Add CI E2E tests (#37)
    * Test attempt 1
    
    * Fix indent
    
    * Add on
    
    * Fix workdir
    
    * Remove composer step
    
    * More updates
    
    * Fix occ scan
    
    * Increase timeouts
    
    * Add ss
    
    * Adjust timeouts again
    
    * Add ss artifact
    
    * Fix report path
    
    * Use index.php
    
    * Remove full group option
    
    * Add matrix to report
    
    * Fix indent
    
    * Github reporter
    
    * Set nc debug flag
    
    * Fix indent again
    
    * Indent
    
    * OKAY
    
    * Lowercase workflow names
    
    * Wth
    
    * Enable viewer
    
    * Substr for attrib
    
    * Fix folders regex
    
    * Fix del test
    
    * Attempt 2
    
    * Move to sh
    
    * Actuall run them
    
    * Docker
    
    * Minimal NC
    
    * Add other db tests
    
    * Fix pg user
    
    * Sequential jobs
    
    * Add job dep
    
    * Restore node

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,33 +13,33 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 600 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 30000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
+  reporter: 'html',
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
+    actionTimeout: 30000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
