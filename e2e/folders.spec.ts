951d033408c1a3d87ec82166e04cd42589de0d62
Varun Patil <radialapps@gmail.com>
2022-09-14 13:52:05 -0700

    Fix folders regex

--- e2e/folders.spec.ts
+++ e2e/folders.spec.ts
@@ -5,12 +5,12 @@ test.beforeEach(login('/folders'));
 
 test.describe('Open', () => {
   test('Look for Folders', async ({ page }) => {
-    expect(await page.locator('.big-icon > .icon-folder').count(), 'Number of folders').toBeGreaterThan(3);
+    expect(await page.locator('.big-icon > .icon-folder').count(), 'Number of folders').toBe(2);
   });
 
   test('Open folder', async ({ page }) => {
     await page.locator('text=Local').click();
-    await expect(page).toHaveURL(/http:\/\/localhost:8080\/apps\/memories\/folders\/\.*/);
+    await expect(page).toHaveURL(/\/apps\/memories\/folders\/[0-9]*/);
     await page.waitForSelector('img[src*="core/preview"]');
   });
 });
