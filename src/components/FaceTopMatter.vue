2abba9da5ddc23bc9e3d36e3418c6a37085a4c0f
Varun Patil <varunpatil@ucla.edu>
2022-10-10 20:40:58 -0700

    Add face name change modal (fix #64)

--- src/components/FaceTopMatter.vue
+++ src/components/FaceTopMatter.vue
@@ -6,7 +6,19 @@
                 <template #icon> <BackIcon :size="20" /> </template>
 			</NcActionButton>
 		</NcActions>
-        <span class="name">{{ name }}</span>
+
+        <div class="name">{{ name }}</div>
+
+        <div class="right-actions">
+            <NcActions>
+                <NcActionButton :aria-label="t('memories', 'Rename person')" @click="showEditModal=true">
+                    {{ t('memories', 'Rename person') }}
+                    <template #icon> <EditIcon :size="20" /> </template>
+                </NcActionButton>
+            </NcActions>
+        </div>
+
+        <FaceEditModal v-if="showEditModal" @close="showEditModal=false" />
     </div>
 </template>
 
@@ -15,17 +27,22 @@ import { Component, Mixins, Watch } from 'vue-property-decorator';
 import GlobalMixin from '../mixins/GlobalMixin';
 
 import { NcActions, NcActionButton } from '@nextcloud/vue';
+import FaceEditModal from './FaceEditModal.vue';
 import BackIcon from 'vue-material-design-icons/ArrowLeft.vue';
+import EditIcon from 'vue-material-design-icons/Pencil.vue';
 
 @Component({
     components: {
         NcActions,
         NcActionButton,
+        FaceEditModal,
         BackIcon,
+        EditIcon,
     },
 })
 export default class FaceTopMatter extends Mixins(GlobalMixin) {
     private name: string = '';
+    private showEditModal: boolean = false;
 
     @Watch('$route')
     async routeChange(from: any, to: any) {
@@ -48,16 +65,23 @@ export default class FaceTopMatter extends Mixins(GlobalMixin) {
 
 <style lang="scss" scoped>
 .face-top-matter {
+    display: flex;
+    vertical-align: middle;
+
     .name {
         font-size: 1.3em;
         font-weight: 400;
-        line-height: 42px;
-        display: inline-block;
-        vertical-align: top;
+        line-height: 40px;
+        padding-left: 3px;
+        flex-grow: 1;
     }
 
-    button {
-        display: inline-block;
+    .right-actions {
+        margin-right: 40px;
+        z-index: 50;
+        @media (max-width: 768px) {
+            margin-right: 10px;
+        }
     }
 }
 </style>
