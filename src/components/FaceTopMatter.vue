218c0f5aebbc712e54ccbe0d4b38b0aa3844ffb0
Varun Patil <varunpatil@ucla.edu>
2022-10-10 20:54:41 -0700

    Add face remove box

--- src/components/FaceTopMatter.vue
+++ src/components/FaceTopMatter.vue
@@ -10,15 +10,20 @@
         <div class="name">{{ name }}</div>
 
         <div class="right-actions">
-            <NcActions>
+            <NcActions :inline="1">
                 <NcActionButton :aria-label="t('memories', 'Rename person')" @click="showEditModal=true">
                     {{ t('memories', 'Rename person') }}
                     <template #icon> <EditIcon :size="20" /> </template>
+                </NcActionButton>
+                <NcActionButton :aria-label="t('memories', 'Remove person')" @click="showDeleteModal=true">
+                    {{ t('memories', 'Remove person') }}
+                    <template #icon> <DeleteIcon :size="20" /> </template>
                 </NcActionButton>
             </NcActions>
         </div>
 
         <FaceEditModal v-if="showEditModal" @close="showEditModal=false" />
+        <FaceDeleteModal v-if="showDeleteModal" @close="showDeleteModal=false" />
     </div>
 </template>
 
@@ -28,21 +33,26 @@ import GlobalMixin from '../mixins/GlobalMixin';
 
 import { NcActions, NcActionButton } from '@nextcloud/vue';
 import FaceEditModal from './FaceEditModal.vue';
+import FaceDeleteModal from './FaceDeleteModal.vue';
 import BackIcon from 'vue-material-design-icons/ArrowLeft.vue';
 import EditIcon from 'vue-material-design-icons/Pencil.vue';
+import DeleteIcon from 'vue-material-design-icons/Close.vue';
 
 @Component({
     components: {
         NcActions,
         NcActionButton,
         FaceEditModal,
+        FaceDeleteModal,
         BackIcon,
         EditIcon,
+        DeleteIcon,
     },
 })
 export default class FaceTopMatter extends Mixins(GlobalMixin) {
     private name: string = '';
     private showEditModal: boolean = false;
+    private showDeleteModal: boolean = false;
 
     @Watch('$route')
     async routeChange(from: any, to: any) {
