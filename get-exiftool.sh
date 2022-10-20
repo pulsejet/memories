dd1466066d06812a95ff7681175cdd3452c1ee14
Varun Patil <varunpatil@ucla.edu>
2022-10-20 13:41:34 -0700

    Also bundle perl script

--- get-exiftool.sh
+++ get-exiftool.sh
@@ -10,4 +10,11 @@ wget "https://github.com/pulsejet/exiftool-bin/releases/download/$exifver/exifto
 wget "https://github.com/pulsejet/exiftool-bin/releases/download/$exifver/exiftool-aarch64-musl"
 wget "https://github.com/pulsejet/exiftool-bin/releases/download/$exifver/exiftool-aarch64-glibc"
 chmod 755 *
+
+wget "https://github.com/exiftool/exiftool/archive/refs/tags/$exifver.zip"
+unzip "$exifver.zip"
+mv "exiftool-$exifver" exiftool
+rm -rf *.zip exiftool/t exiftool/html
+chmod 755 exiftool/exiftool
+
 cd ..
