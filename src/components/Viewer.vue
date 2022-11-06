169b057de1236b21a5ec5f5a4b49d3a417fabd32
Varun Patil <varunpatil@ucla.edu>
2022-11-05 19:05:24 -0700

    viewer: remove commented code

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -233,66 +233,6 @@ export default class Viewer extends Mixins(GlobalMixin) {
 
   /** Open using start photo and rows list */
   public async open(anchorPhoto: IPhoto, rows?: IRow[]) {
-    //   list = list || photo.d?.detail;
-    //   if (!list?.length) return;
-    //   // Repopulate map
-    //   this.photoMap.clear();
-    //   for (const p of list) {
-    //     this.photoMap.set(p.fileid, p);
-    //   }
-    //   // Get file infos
-    //   let fileInfos: IFileInfo[];
-    //   try {
-    //     this.updateLoading(1);
-    //     fileInfos = await dav.getFiles(list);
-    //   } catch (e) {
-    //     console.error("Failed to load fileInfos", e);
-    //     showError("Failed to load fileInfos");
-    //     return;
-    //   } finally {
-    //     this.updateLoading(-1);
-    //   }
-    //   if (fileInfos.length === 0) {
-    //     return;
-    //   }
-    //   // Fix sorting of the fileInfos
-    //   const itemPositions = {};
-    //   for (const [index, p] of list.entries()) {
-    //     itemPositions[p.fileid] = index;
-    //   }
-    //   fileInfos.sort(function (a, b) {
-    //     return itemPositions[a.fileid] - itemPositions[b.fileid];
-    //   });
-    //   // Get this photo in the fileInfos
-    //   const fInfo = fileInfos.find((d) => Number(d.fileid) === photo.fileid);
-    //   if (!fInfo) {
-    //     showError(t("memories", "Cannot find this photo anymore!"));
-    //     return;
-    //   }
-    //   // Check viewer > 2.0.0
-    //   const viewerVersion: string = globalThis.OCA.Viewer.version;
-    //   const viewerMajor = Number(viewerVersion.split(".")[0]);
-    //   // Open Nextcloud viewer
-    //   globalThis.OCA.Viewer.open({
-    //     fileInfo: fInfo,
-    //     path: viewerMajor < 2 ? fInfo.filename : undefined, // Only specify path upto Nextcloud 24
-    //     list: fileInfos, // file list
-    //     canLoop: false, // don't loop
-    //     onClose: () => {
-    //       // on viewer close
-    //       if (globalThis.OCA.Files.Sidebar.file) {
-    //         localStorage.setItem(SIDEBAR_KEY, "1");
-    //       } else {
-    //         localStorage.removeItem(SIDEBAR_KEY);
-    //       }
-    //       globalThis.OCA.Files.Sidebar.close();
-    //     },
-    //   });
-    //   // Restore sidebar state
-    //   if (localStorage.getItem(SIDEBAR_KEY) === "1") {
-    //     globalThis.OCA.Files.Sidebar.open(fInfo.filename);
-    //   }
-
     this.list = [...anchorPhoto.d.detail];
     let startIndex = -1;
 
