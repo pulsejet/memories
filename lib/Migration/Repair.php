b14a01962f488a857360093fba5aff57c7fe2e32
Varun Patil <varunpatil@ucla.edu>
2022-11-10 22:18:15 -0800

    Add repair steps

---
+++ lib/Migration/Repair.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Memories\Migration;
+
+use OCP\IConfig;
+use OCP\Migration\IOutput;
+use OCP\Migration\IRepairStep;
+
+class Repair implements IRepairStep {
+
+	protected IConfig $config;
+
+	public function __construct(IConfig $config) {
+		$this->config = $config;
+	}
+
+	public function getName(): string {
+		return 'Repair steps for Memories';
+	}
+
+	public function run(IOutput $output): void {
+		// kill any instances of go-transcode and go-vod
+        shell_exec('pkill go-transcode');
+        shell_exec('pkill go-vod');
+	}
+}
