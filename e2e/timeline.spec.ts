2e7baa05ab0f7a61f834edc2aa7c174e9e829b3c
Varun Patil <varunpatil@ucla.edu>
2022-12-07 15:50:51 -0800

    test: add extra refresh

--- e2e/timeline.spec.ts
+++ e2e/timeline.spec.ts
@@ -50,6 +50,8 @@ test.describe("Open", () => {
     // refresh page
     await page.reload();
     await page.waitForTimeout(4000); // cache
+    await page.reload(); // prevent stale cache issues
+    await page.waitForTimeout(4000); // cache
     await page.waitForSelector('img[src*="api/image/preview"]');
     expect(await page.locator(`img[src="${src1}"]`).count()).toBe(0);
     expect(await page.locator(`img[src="${src2}"]`).count()).toBe(0);
