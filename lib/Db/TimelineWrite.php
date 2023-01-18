b18a098f47ea3e09a8fe8b5863557a0d6dae2ace
Varun Patil <varunpatil@ucla.edu>
2023-01-17 20:52:26 -0800

    index: allow cleaning up orphans (fix #326)

--- lib/Db/TimelineWrite.php
+++ lib/Db/TimelineWrite.php
@@ -222,4 +222,47 @@ class TimelineWrite
         $t2 = $p->getTruncateTableSQL('`*PREFIX*memories_livephoto`', false);
         $this->connection->executeStatement("{$t1}; {$t2}");
     }
+
+    /**
+     * Mark a file as not orphaned.
+     */
+    public function unorphan(File &$file)
+    {
+        $query = $this->connection->getQueryBuilder();
+        $query->update('memories')
+            ->set('orphan', $query->createNamedParameter(false, IQueryBuilder::PARAM_BOOL))
+            ->where($query->expr()->eq('fileid', $query->createNamedParameter($file->getId(), IQueryBuilder::PARAM_INT)))
+        ;
+        $query->executeStatement();
+    }
+
+    /**
+     * Mark all files in the table as orphaned.
+     *
+     * @return int Number of rows affected
+     */
+    public function orphanAll(): int
+    {
+        $query = $this->connection->getQueryBuilder();
+        $query->update('memories')
+            ->set('orphan', $query->createNamedParameter(true, IQueryBuilder::PARAM_BOOL))
+        ;
+
+        return $query->executeStatement();
+    }
+
+    /**
+     * Remove all entries that are orphans.
+     *
+     * @return int Number of rows affected
+     */
+    public function removeOrphans(): int
+    {
+        $query = $this->connection->getQueryBuilder();
+        $query->delete('memories')
+            ->where($query->expr()->eq('orphan', $query->createNamedParameter(true, IQueryBuilder::PARAM_BOOL)))
+        ;
+
+        return $query->executeStatement();
+    }
 }
