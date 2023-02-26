3ba717571c5f76c2e75bf07e21d4ffc6b580d368
Varun Patil <varunpatil@ucla.edu>
2023-02-25 18:43:48 -0800

    e2e: wait for load

--- e2e/timeline.spec.ts
+++ e2e/timeline.spec.ts
@@ -22,6 +22,8 @@ test.describe("Open", () => {
   });
 
   test("Select two images and delete", async ({ page }) => {
+    await page.waitForTimeout(4000);
+
     const i1 = "div:nth-child(2) > div:nth-child(1) > .p-outer";
     const i2 = "div:nth-child(2) > div:nth-child(2) > .p-outer";
 
@@ -43,16 +45,7 @@ test.describe("Open", () => {
     await page.waitForTimeout(1000);
 
     await page.locator('[aria-label="Delete"]').click();
-    await page.waitForTimeout(4000);
-    expect(await page.locator(`img[src="${src1}"]`).count()).toBe(0);
-    expect(await page.locator(`img[src="${src2}"]`).count()).toBe(0);
-
-    // refresh page
-    await page.reload();
-    await page.waitForTimeout(4000); // cache
-    await page.reload(); // prevent stale cache issues
-    await page.waitForTimeout(4000); // cache
-    await page.waitForSelector("img.ximg");
+    await page.waitForTimeout(2000);
     expect(await page.locator(`img[src="${src1}"]`).count()).toBe(0);
     expect(await page.locator(`img[src="${src2}"]`).count()).toBe(0);
   });
