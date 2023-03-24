13c4ed97eb3707dee80b68ad74342d935ace1b62
Varun Patil <varunpatil@ucla.edu>
2023-03-23 22:10:19 -0700

    e2e: revert pwd change
    Signed-off-by: Varun Patil <varunpatil@ucla.edu>

--- e2e/login.ts
+++ e2e/login.ts
@@ -8,7 +8,7 @@ export function login(route: string) {
     await page.locator("#user").click();
     await page.locator("#user").fill("admin");
     await page.locator("#user").press("Tab");
-    await page.locator("#password").fill("admin");
+    await page.locator("#password").fill("password");
     await page.locator('button[type="submit"]').click();
     await expect(page).toHaveURL(
       "http://localhost:8080/index.php/apps/memories" + route
