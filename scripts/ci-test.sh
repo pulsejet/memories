117e8490386dd63fa3747e9d2025688461f117e9
Varun Patil <varunpatil@ucla.edu>
2023-04-10 17:12:06 -0700

    ci: fix repair
    Signed-off-by: Varun Patil <varunpatil@ucla.edu>

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -17,9 +17,17 @@ php occ app:disable updatenotification
 php occ app:disable systemtags
 php occ app:disable files_sharing
 
+# Setup
+cd apps/memories
+make exiftool
+cd ../..
+
 # Enable apps
 php occ app:enable --force viewer
 php occ app:enable --force memories
+
+# Run repair steps
+php occ maintenance:repair
 
 set -e
 
@@ -32,11 +40,6 @@ cd data/admin/files
 wget https://github.com/pulsejet/memories-assets/raw/main/Files.zip
 unzip Files.zip
 cd ../../..
-
-# Setup
-cd apps/memories
-make exiftool
-cd ../..
 
 # Index
 php occ files:scan --all
