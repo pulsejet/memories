e685d114bf5e882d0a460fc014a19f94fab17869
Varun Patil <me@varunpatil.me>
2023-04-12 13:20:57 -0700

    Update build files

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 apt-get update
 apt-get remove -y libva ffmpeg
 apt-get install -y \
@@ -8,12 +10,13 @@ apt-get install -y \
     libx11-dev libgl1-mesa-glx libgl1-mesa-dev \
     xcb libxcb-xkb-dev x11-xkb-utils libx11-xcb-dev \
     libxkbcommon-x11-dev libxcb-dri3-dev \
-    cmake git nasm build-essential
+    cmake git nasm build-essential \
+    libx264-dev
 
 mkdir qsvbuild
 cd qsvbuild
 
-git clone --depth 1 --branch 2.17.0 https://github.com/intel/libva
+git clone --depth 1 --branch 2.18.0 https://github.com/intel/libva
 cd libva
 ./autogen.sh
 make
@@ -21,7 +24,7 @@ sudo make install
 sudo ldconfig
 cd ..
 
-git clone --depth 1 --branch intel-gmmlib-22.3.4 https://github.com/intel/gmmlib
+git clone --depth 1 --branch intel-gmmlib-22.3.5 https://github.com/intel/gmmlib
 cd gmmlib
 mkdir build && cd build
 cmake ..
@@ -30,7 +33,7 @@ sudo make install
 sudo ldconfig
 cd ../..
 
-git clone --depth 1 --branch intel-media-22.6.6 https://github.com/intel/media-driver
+git clone --depth 1 --branch intel-media-23.1.6 https://github.com/intel/media-driver
 mkdir -p build_media
 cd build_media
 cmake ../media-driver
@@ -41,7 +44,11 @@ cd ..
 
 git clone --depth 1 --branch n6.0 https://github.com/FFmpeg/FFmpeg
 cd FFmpeg
-./configure --enable-nonfree
+./configure \
+	--enable-nonfree \
+	--enable-gpl \
+	--enable-libx264
+
 make -j"$(nproc)"
 sudo make install
 sudo ldconfig
