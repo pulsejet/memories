105987e92d85e6135164978bc263d3920d26a611
Varun Patil <radialapps@gmail.com>
2023-04-19 17:07:37 -0700

    dav: fix escaping in folder url (fix #569)
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- src/services/dav/other.ts
+++ src/services/dav/other.ts
@@ -1,6 +1,7 @@
 import { getFiles } from './base';
 import { generateUrl } from '@nextcloud/router';
 import { IPhoto } from '../../types';
+import { dirname } from 'path';
 
 /**
  * Open the files app with the given photo
@@ -11,7 +12,9 @@ export async function viewInFolder(photo: IPhoto) {
   if (f.length === 0) return;
 
   const file = f[0];
-  const dirPath = file.filename.split('/').slice(0, -1).join('/');
-  const url = generateUrl(`/apps/files/?dir=${dirPath}&scrollto=${file.fileid}&openfile=${file.fileid}`);
+  const url = generateUrl(`/apps/files/?dir={dirPath}&scrollto={fileid}&openfile={fileid}`, {
+    dirPath: dirname(file.filename),
+    fileid: file.fileid,
+  });
   window.open(url, '_blank');
 }
