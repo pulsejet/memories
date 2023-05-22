8341458f80d4c0430f70b9629a900515e58c4c5f
Varun Patil <radialapps@gmail.com>
2023-05-21 21:45:42 -0700

    welcome: more to sync

---
+++ app/src/main/assets/sync.js
@@ -0,0 +1,54 @@
+const main = document.getElementById("main");
+const waiting = document.getElementById("waiting");
+const sync = document.getElementById("sync");
+const localFolders = document.getElementById("local-folders");
+
+// Waiting page => sync page
+window.loggedIn = async () => {
+  waiting.classList.add("invisible");
+  await new Promise((resolve) => setTimeout(resolve, 700));
+  waiting.remove();
+  sync.classList.remove("invisible");
+};
+
+// Local list of folders
+let localList = null;
+async function openLocal() {
+  // Get the list of local folders for next screen
+  (async () => {
+    const res = await fetch("http://127.0.0.1/api/config/local-folders");
+    localList = await res.json();
+
+    // Add HTML for folders list
+    document.getElementById("folder-list").innerHTML = localList
+      .map(
+        (folder) => `
+              <div class="folder-choose">
+                  <input type="checkbox" id="folder-${folder.id}" ${folder.enabled ? "checked" : ""}>
+                  <label for="${folder.id}">${folder.name}</label>
+              </div>
+          `
+      )
+      .join("");
+  })();
+
+  // Show the folders list
+  sync.classList.add("invisible");
+  await new Promise((resolve) => setTimeout(resolve, 700));
+  sync.remove();
+  localFolders.classList.remove("invisible");
+}
+
+// Open main app
+async function start() {
+  // Mark all checked as enabled
+  if (localList) {
+    localList.forEach((f) => (f.enabled = document.getElementById(`folder-${f.id}`).checked));
+    window.nativex?.configSetLocalFolders(JSON.stringify(localList));
+  }
+
+  // Start the app
+  main.classList.add("invisible");
+  await new Promise((resolve) => setTimeout(resolve, 700));
+  window.nativex?.reload();
+}
