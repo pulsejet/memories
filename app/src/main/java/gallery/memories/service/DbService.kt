5355dc5b3892c3b672c464dc2966da15f8737a9a
Varun Patil <radialapps@gmail.com>
2023-05-21 19:26:04 -0700

    db: reset sync time on version change

--- app/src/main/java/gallery/memories/service/DbService.kt
+++ app/src/main/java/gallery/memories/service/DbService.kt
@@ -3,8 +3,9 @@ package gallery.memories.service
 import android.content.Context
 import android.database.sqlite.SQLiteOpenHelper
 import android.database.sqlite.SQLiteDatabase
+import gallery.memories.R
 
-class DbService(context: Context) : SQLiteOpenHelper(context, "memories", null, 27) {
+class DbService(val context: Context) : SQLiteOpenHelper(context, "memories", null, 31) {
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL("""
             CREATE TABLE images (
@@ -27,6 +28,12 @@ class DbService(context: Context) : SQLiteOpenHelper(context, "memories", null, 
 
     override fun onUpgrade(database: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         database.execSQL("DROP TABLE IF EXISTS images")
+
+        // Reset sync time
+        context.getSharedPreferences(context.getString(R.string.preferences_key), 0).edit()
+            .remove(context.getString(R.string.preferences_last_sync_time))
+            .apply()
+
         onCreate(database)
     }
 }
