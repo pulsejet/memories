359f9a97ad08e29d2ea3ee40744b55916af45a78
Varun Patil <radialapps@gmail.com>
2023-05-28 00:12:31 -0700

    timeline: restore subpixel rounding fix
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1313,7 +1313,7 @@ export default defineComponent({
     // Get rid of padding on img-outer (1px on mobile)
     // Also need to make sure we don't end up with a scrollbar -- see below
     margin-left: -1px;
-    width: calc(100% + 2px);
+    width: calc(100% + 3px); // 1px extra here for sub-pixel rounding
   }
 }
 
