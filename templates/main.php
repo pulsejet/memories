2a54057c850fbc680ef9a637c4bdb8310e55a5ec
Varun Patil <radialapps@gmail.com>
2023-06-02 19:30:03 -0700

    template: fix php tag
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- templates/main.php
+++ templates/main.php
@@ -1,9 +1,9 @@
-<? if ($native === true) { ?>
+<?php if ($native === true) { ?>
 <style type="text/css">
     body, html {
         background: transparent;
     }
 </style>
-<? } ?>
+<?php } ?>
 
 <div id="vue-content"></div>
