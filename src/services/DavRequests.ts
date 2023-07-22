ec8a2baed0ea404e15d2d6177dc613a459ee79fa
Varun Patil <radialapps@gmail.com>
2023-07-22 13:04:29 -0700

    chore: bump up dependencies
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- src/services/DavRequests.ts
+++ src/services/DavRequests.ts
@@ -4,7 +4,6 @@ export * from './dav/archive';
 export * from './dav/download';
 export * from './dav/face';
 export * from './dav/favorites';
-export * from './dav/folders';
 export * from './dav/onthisday';
 export * from './dav/tags';
 export * from './dav/other';
