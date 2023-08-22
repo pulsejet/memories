ba19a3b45fdccc729c145d87622cd57e37448062
Varun Patil <radialapps@gmail.com>
2023-08-21 17:35:05 -0700

    dialog: allow blank title and message
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- src/services/utils/dialog.ts
+++ src/services/utils/dialog.ts
@@ -2,9 +2,9 @@ import { translate as t } from '@nextcloud/l10n';
 
 type ConfirmOptions = {
   /** Title of dialog */
-  title: string;
+  title?: string;
   /** Message to display */
-  message: string;
+  message?: string;
   /** Type of dialog (default YES_NO_BUTTONS) */
   type?: string;
   /** Text for confirm button (default "Yes") */
@@ -13,13 +13,15 @@ type ConfirmOptions = {
   confirmClasses?: 'error' | 'primary';
   /** Text for cancel button (default "No") */
   cancel?: string;
-  /** Classes to add to cancel button */
-  cancelClasses?: 'error' | 'primary';
+  /** Whether to show a modal dialog (default true) */
+  modal?: boolean;
 };
 
 export function confirmDestructive(options: ConfirmOptions): Promise<boolean> {
   const opts = Object.assign(
     {
+      title: '',
+      message: '',
       type: (<any>OC.dialogs).YES_NO_BUTTONS,
       confirm: t('memories', 'Yes'),
       confirmClasses: 'error',
