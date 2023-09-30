f76703ee7fa80432188485800841625b0b9d75b4
Varun Patil <radialapps@gmail.com>
2023-09-30 11:56:55 -0700

    test: fix E2E

--- e2e/folders.spec.ts
+++ e2e/folders.spec.ts
@@ -1,23 +1,23 @@
-import { test, expect } from "@playwright/test";
-import { login } from "./login";
+import { test, expect } from '@playwright/test';
+import { login } from './login';
 
-test.beforeEach(login("/folders"));
+test.beforeEach(login('/folders'));
 
-test.describe("Open", () => {
+test.describe('Open', () => {
   test.beforeEach(async ({ page }) => {
-    await page.waitForSelector(".big-icon");
+    await page.waitForSelector('.big-icon');
     await page.waitForTimeout(500);
   });
 
-  test("Look for Folders", async ({ page }) => {
-    const ct = await page.locator(".big-icon").count();
-    expect(ct, "Number of folders").toBe(2);
+  test('Look for Folders', async ({ page }) => {
+    const ct = await page.locator('.big-icon').count();
+    expect(ct, 'Number of folders').toBe(2);
   });
 
-  test("Open folder", async ({ page }) => {
-    await page.locator("text=Local").click();
+  test('Open folder', async ({ page }) => {
+    await page.locator('text=Local').click();
     await page.waitForTimeout(2000);
-    const elems = await page.locator(".img-outer").all();
+    const elems = await page.locator('.img-outer').all();
     expect(elems.length).toBeGreaterThanOrEqual(3);
   });
 });
