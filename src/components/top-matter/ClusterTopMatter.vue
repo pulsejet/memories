94053f7dd3912dfa6b5dd3e60a0258c6001bebe8
Varun Patil <radialapps@gmail.com>
2023-09-29 19:25:39 -0700

    refactor: fix PR
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- src/components/top-matter/ClusterTopMatter.vue
+++ src/components/top-matter/ClusterTopMatter.vue
@@ -13,7 +13,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import { translate as t } from '@nextcloud/l10n';
 import NcActions from '@nextcloud/vue/dist/Components/NcActions';
 import NcActionButton from '@nextcloud/vue/dist/Components/NcActionButton';
 import * as strings from '../../services/strings';
@@ -36,7 +35,7 @@ export default defineComponent({
     name(): string | null {
       switch (this.$route.name) {
         case 'tags':
-          return t('recognize',this.$route.params.name);
+          return this.t('recognize', this.$route.params.name);
         case 'places':
           return this.$route.params.name?.split('-').slice(1).join('-');
         default:
