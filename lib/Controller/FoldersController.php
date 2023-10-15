eee5abf2103eb799e553e15bc88ae816406fd419
Varun Patil <radialapps@gmail.com>
2023-10-14 19:34:30 -0700

    refactor: improve typing
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- lib/Controller/FoldersController.php
+++ lib/Controller/FoldersController.php
@@ -31,9 +31,14 @@ class FoldersController extends GenericApiController
 
             // Ugly: get the view of the folder with reflection
             // This is unfortunately the only way to get the contents of a folder
-            // matching a MIME type without using SEARCH, which is deep
-            $rp = new \ReflectionProperty('\OC\Files\Node\Node', 'view');
+            // matching a MIME type without using SEARCH, which is deep.
+            //
+            // To top it off, all this is completely useless at the moment
+            // because the MIME search is done PHP-side
+            $rp = new \ReflectionProperty(\OC\Files\Node\Node::class, 'view');
             $rp->setAccessible(true);
+
+            /** @var \OC\Files\View */
             $view = $rp->getValue($node);
 
             // Get the subfolders
