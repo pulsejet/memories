27b252ce9cc89a31e49ff739b586e963849efc57
Varun Patil <radialapps@gmail.com>
2023-10-18 12:21:16 -0700

    build: remove babel
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- webpack.js
+++ webpack.js
@@ -1,74 +1,140 @@
 const webpack = require('webpack');
 const path = require('path');
-const webpackConfig = require('@nextcloud/webpack-vue-config');
+
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
+const appName = process.env.npm_package_name;
+const appVersion = process.env.npm_package_version;
 const buildMode = process.env.NODE_ENV;
 const isDev = buildMode === 'development';
+console.info('Building', appName, appVersion, '\n');
 
-// Entry points
-webpackConfig.entry = {
-  main: path.resolve(path.join('src', 'main')),
-  admin: path.resolve(path.join('src', 'admin')),
-  'hooks-clear-cache': path.resolve(path.join('src', 'hooks', 'clear-cache')),
-};
+module.exports = {
+  target: 'web',
+  mode: buildMode,
+  devtool: isDev ? 'cheap-source-map' : false,
+  cache: isDev,
 
-// Enable TypeScript for Vue
-const tsRule = webpackConfig.module.rules.find((rule) => rule.use?.includes('ts-loader'));
-console.assert(tsRule, 'Could not find ts-loader rule');
-tsRule.use = [
-  { loader: 'babel-loader' },
-  {
-    loader: 'ts-loader',
-    options: {
-      appendTsSuffixTo: [/\.vue$/],
+  entry: {
+    main: path.resolve(path.join(__dirname, 'src', 'main')),
+    admin: path.resolve(path.join(__dirname, 'src', 'admin')),
+    'hooks-clear-cache': path.resolve(path.join(__dirname, 'src', 'hooks', 'clear-cache')),
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    publicPath: path.join('/apps/', appName, '/js/'),
+
+    // Output file names
+    filename: `${appName}-[name].js?v=[contenthash]`,
+    chunkFilename: `${appName}-[name].js?v=[contenthash]`,
+
+    // Clean output before each build
+    clean: true,
+
+    // Make sure sourcemaps have a proper path and do not
+    // leak local paths https://github.com/webpack/webpack/issues/3603
+    devtoolNamespace: appName,
+    devtoolModuleFilenameTemplate(info) {
+      const rootDir = process.cwd();
+      const rel = path.relative(rootDir, info.absoluteResourcePath);
+      return `webpack:///${appName}/${rel}`;
     },
   },
-];
 
-// Exclude node_modules from watch
-webpackConfig.watchOptions = {
-  ignored: /node_modules/,
-  aggregateTimeout: 300,
-};
+  watchOptions: {
+    ignored: /node_modules/,
+    aggregateTimeout: 300,
+  },
 
-// Bundle service worker
-webpackConfig.plugins.push(
-  new WorkboxPlugin.InjectManifest({
-    swSrc: path.resolve(path.join('src', 'service-worker.js')),
-    swDest: 'memories-service-worker.js',
-  }),
-);
-
-// Minification
-webpackConfig.optimization.minimizer = [
-  new TerserPlugin({
-    exclude: [/filerobot-image-editor/],
-    terserOptions: {
-      output: {
-        comments: false,
-      },
+  optimization: {
+    chunkIds: 'named',
+    splitChunks: {
+      automaticNameDelimiter: '-',
     },
-    extractComments: true,
-  }),
-];
+    minimize: !isDev,
+    minimizer: [
+      new TerserPlugin({
+        exclude: [/filerobot-image-editor/],
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: true,
+      }),
+    ],
+  },
 
-// Disable source maps in production
-webpackConfig.devtool = isDev ? 'cheap-source-map' : false;
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              appendTsSuffixTo: [/\.vue$/],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
 
-// Configure source map public path
-webpackConfig.plugins.push(
-  new webpack.SourceMapDevToolPlugin({
-    filename: '[file].map',
-    publicPath: path.join('/apps/', process.env.npm_package_name, '/js/'),
-  }),
-);
+  plugins: [
+    new VueLoaderPlugin(),
 
-// Enable caching
-webpackConfig.cache = true;
+    // Make sure we auto-inject node polyfills on demand
+    // https://webpack.js.org/blog/2020-10-10-webpack-5-release/#automatic-nodejs-polyfills-removed
+    new NodePolyfillPlugin({
+      includeAliases: ['stream', 'process'], // webdav
+    }),
 
-// Bundle analyzer (npm i --no-save webpack-bundle-analyzer)
-// webpackConfig.plugins.push(new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)());
+    // Configure source map public path
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map',
+      publicPath: path.join('/apps/', process.env.npm_package_name, '/js/'),
+    }),
 
-module.exports = webpackConfig;
+    // Bundle service worker
+    new WorkboxPlugin.InjectManifest({
+      swSrc: path.resolve(path.join('src', 'service-worker.js')),
+      swDest: 'memories-service-worker.js',
+    }),
+
+    // Make appName & appVersion available as a constant
+    new webpack.DefinePlugin({ appName: JSON.stringify(appName) }),
+    new webpack.DefinePlugin({ appVersion: JSON.stringify(appVersion) }),
+
+    // Bundle analyzer (npm i --no-save webpack-bundle-analyzer)
+    // new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)()
+  ],
+
+  resolve: {
+    extensions: ['.ts', '.js', '.vue'],
+    symlinks: false,
+    // Ensure npm does not duplicate vue dependency, and that npm link works for vue 3
+    // See https://github.com/vuejs/core/issues/1503
+    // See https://github.com/nextcloud/nextcloud-vue/issues/3281
+    alias: {
+      vue$: path.resolve('./node_modules/vue'),
+    },
+  },
+};
