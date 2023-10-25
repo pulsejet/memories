134b7cc3545096b57801f4dd3449227d8053ae61
Varun Patil <radialapps@gmail.com>
2023-10-24 19:35:55 -0700

    bin-ext: bump go-vod
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- lib/Service/BinExt.php
+++ lib/Service/BinExt.php
@@ -9,7 +9,7 @@ use OCA\Memories\Settings\SystemConfig;
 class BinExt
 {
     public const EXIFTOOL_VER = '12.60';
-    public const GOVOD_VER = '0.1.23';
+    public const GOVOD_VER = '0.1.24';
     public const NX_VER_MIN = '1.1';
 
     /** Get the path to the temp directory */
