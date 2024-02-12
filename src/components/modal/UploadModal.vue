ea33d69c6028143974279644f1cb5471dd5c9bea
Alexander Saltykov <temp.kroogi@gmail.com>
2024-02-12 15:51:49 +0300

    Adds file selector and previews
    Signed-off-by: Alexander Saltykov <temp.kroogi@gmail.com>

--- src/components/modal/UploadModal.vue
+++ src/components/modal/UploadModal.vue
@@ -4,9 +4,21 @@
             {{ t('memories', 'Upload') }}
         </template>
         <div class="photos">
-            <NcButton @click="select" class="button" :disabled="processing">
-                {{ t('memories', 'Select photos') }}
-            </NcButton>
+            <VueUploadComponent :multiple="true" ref="upload" v-model="dummy" @input-file="select" @input-filter="filter">
+                <NcButton @click="select" class="button" :disabled="processing">
+                    {{ t('memories', 'Select photos') }}
+                </NcButton>
+            </VueUploadComponent>
+            <div class="previews">
+                <div v-for="photo in photos">
+                    <div class="preview">
+                        <img :src=photo.preview width="50" height="50" />
+                        <div class="deleteIcon" @click="removePhoto" :data-id=photo.id>
+                            <Delete />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="subtitle" @click="showAlbums">
             <div class="icon">
@@ -16,7 +28,7 @@
             {{ t('memories', 'Add to albums') }}
         </div>
         <div v-bind:class="{ spoiler: true, opened: albumsShown }">
-            <AlbumPicker hideSaveBtn @toggleAlbum="toggleAlbum" :photos="photos" :disabled="processing" />
+            <AlbumPicker hideSaveBtn @toggleAlbum="toggleAlbum" :photos="dummy" :disabled="processing" />
         </div>
         <div class="subtitle" @click="showTags">
             <div class="icon">
@@ -26,9 +38,12 @@
             {{ t('memories', 'Assign tags') }}
         </div>
         <div v-bind:class="{ spoiler: true, opened: tagsShown }">
-            <EditTags ref="tags" :photos="photos" :disabled="processing" />
+            <EditTags ref="tags" :photos="dummy" :disabled="processing" />
         </div>
         <div class="actions">
+            <div class="progress-bar" v-if="processing">
+                <NcProgressBar :value="progress" :error="true" />
+            </div>
             <NcButton @click="upload" class="button" type="error" v-if="photos" :disabled="processing">
                 {{ t('memories', 'Upload') }}
             </NcButton>
@@ -37,7 +52,7 @@
 </template>
   
 <script lang="ts">
-import { defineComponent, type PropType } from 'vue';
+import { defineComponent } from 'vue';
 
 import Modal from './Modal.vue';
 import ModalMixin from './ModalMixin';
@@ -47,8 +62,15 @@ import EditTags from './EditTags.vue';
 import NcButton from '@nextcloud/vue/dist/Components/NcButton';
 import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
 import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
+import Delete from 'vue-material-design-icons/Delete.vue';
+import VueUploadComponent from 'vue-upload-component';
+const NcProgressBar = () => import('@nextcloud/vue/dist/Components/NcProgressBar');
 
 import type { IAlbum, IPhoto } from '@typings';
+
+interface Photo extends VUFile {
+    preview: string;
+}
 
 export default defineComponent({
     name: 'UploadModal',
@@ -69,19 +91,25 @@ export default defineComponent({
         EditTags,
         ChevronDown,
         ChevronUp,
+        Delete,
         NcButton,
+        VueUploadComponent,
+        NcProgressBar,
     },
     mixins: [
         ModalMixin,
     ],
 
     data: () => ({
-        photos: [] as IPhoto[],
-        opsTotal: 0,
-        albumsShown: true,
-        tagsShown: true,
-        processing: false,
+        photos: [] as Photo[],
+        dummy: [] as IPhoto[],
+        previews: [],
+        progress: 0,
+        albumsShown: false,
+        tagsShown: false,
         selectedAlbums: [] as IAlbum[],
+        processings: {} as { [key: string | number]: Promise<void> },
+        processing: false,
     }),
 
     mounted() {
@@ -101,12 +129,17 @@ export default defineComponent({
     },
 
     methods: {
-        open(photos: IPhoto[]) {
-            this.photos = photos;
-            this.opsTotal = 0;
+        open() {
         },
 
         onClose() {
+            this.photos = [];
+            this.processings = {};
+            this.progress = 0;
+            this.previews = [];
+            this.albumsShown = false;
+            this.tagsShown = false;
+            this.processing = false;
             this.$emit('onClose');
         },
 
@@ -122,8 +155,35 @@ export default defineComponent({
             this.tagsShown = !this.tagsShown;
         },
 
-        select() {
+        select(newFile: VUFile) {
+            let reader = new FileReader();
+            if (newFile) {
+                reader.readAsDataURL(newFile.file);
+                this.processings[newFile.id] = new Promise((resolve, reject) => {
+                    reader.onload = (e) => {
+                        this.photos.push({
+                            ...newFile,
+                            preview: e.target?.result?.toString() ?? "",
+                        });
+                        resolve();
+                        delete this.processings[newFile.id];
+                        this.processing = Object.keys(this.processings).length > 0;
+                    };
+                    reader.onerror = () => {
+                        reject();
+                        delete this.processings[newFile.id];
+                        this.processing = Object.keys(this.processings).length > 0;
+                    };
+                });
+            }
+        },
 
+        removePhoto(e: MouseEvent) {
+            console.log(e.currentTarget);
+            // this.photos = this.photos.filter((photo) => photo.id === e.target.dataset.id);
+        },
+
+        filter() {
         },
 
         async upload() {
@@ -162,6 +222,26 @@ export default defineComponent({
     display: flex;
     justify-content: flex-end;
     padding: 0.5rem 0 0;
+}
+
+.previews {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.preview {
+    position: relative;
+    margin: 0 5px;
+}
+
+.deleteIcon {
+    top: calc(50% - 20px);
+    position: absolute;
+    left: calc(50% - 15px);
+    background: #ffffff99;
+    border-radius: 50%;
+    padding: 5px;
+    cursor: pointer;
 }
 </style>
   
