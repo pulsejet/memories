94659dff62fbee5c475f7c346275afebb95ae86d
Varun Patil <radialapps@gmail.com>
2024-03-10 13:26:24 -0700

    exif: refactor error message
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- lib/Exif.php
+++ lib/Exif.php
@@ -90,7 +90,7 @@ class Exif
             $path = $file->getStorage()->getLocalFile($file->getInternalPath());
         } catch (\Throwable $ex) {
             // https://github.com/pulsejet/memories/issues/820
-            throw new \Exception('Failed to get local file: '.$ex->getMessage());
+            throw new \Exception("Failed to get local file: {$ex->getMessage()}");
         }
 
         // Check if path is valid
@@ -100,7 +100,7 @@ class Exif
 
         // Check if file is readable
         if (!is_readable($path)) {
-            throw new \Exception('File is not readable');
+            throw new \Exception("File is not readable: {$path}");
         }
 
         $exif = self::getExifFromLocalPath($path);
