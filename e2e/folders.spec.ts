0c605758bc5cf520335b55ce487ae887d1e560d2
Varun Patil <radialapps@gmail.com>
2024-03-29 00:19:08 -0700

    test: use exact count
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- e2e/folders.spec.ts
+++ e2e/folders.spec.ts
@@ -10,14 +10,14 @@ test.describe('Open', () => {
   });
 
   test('Look for Folders', async ({ page }) => {
-    const ct = await page.locator('.big-icon').count();
+    const ct = await page.locator('.big-icon:visible').count();
     expect(ct, 'Number of folders').toBe(2);
   });
 
   test('Open folder', async ({ page }) => {
     await page.locator('text=Local').click();
     await page.waitForTimeout(2000);
-    const elems = await page.locator('.img-outer').all();
-    expect(elems.length).toBeGreaterThanOrEqual(3);
+    const elems = await page.locator('.img-outer:visible').all();
+    expect(elems.length, 'Number of files').toEqual(3);
   });
 });
