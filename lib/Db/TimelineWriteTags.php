e24008005ee2f4a344267aef7bb3d7df620ce114
Jona Löffler <37999659+JonaLoeffler@users.noreply.github.com>
2025-04-02 17:52:10 +0200

    Clean up logging

--- lib/Db/TimelineWriteTags.php
+++ lib/Db/TimelineWriteTags.php
@@ -9,12 +9,13 @@ use OCP\Files\File;
 use Psr\Log\LoggerInterface;
 use OCP\IDBConnection;
 
-trait TimelineWriteTags 
+trait TimelineWriteTags
 {
     protected IDBConnection $connection;
     protected LoggerInterface $logger;
 
-    public function processTags(File $file, array $exif): void{
+    public function processTags(File $file, array $exif): void
+    {
         foreach ($exif["TagsList"] as $tag) {
             $query = $this->connection->getQueryBuilder();
             $exists = $query->select('id')
@@ -23,19 +24,14 @@ trait TimelineWriteTags
                 ->executeQuery()
                 ->fetch();
 
-            $this->logger->error("fetched oc systemtag id", ["exists" => $exists]);
-
             if (!$exists) {
                 $query = $this->connection->getQueryBuilder();
                 $query->insert('systemtag')
                     ->values([
-                         'name' => $query->createNamedParameter($tag, IQueryBuilder::PARAM_STR)
+                        'name' => $query->createNamedParameter($tag, IQueryBuilder::PARAM_STR)
                     ])->executeStatement();
                 $exists = $query->getLastInsertId();
-            } 
-
-            $this->logger->error("created oc systemtag id", ["exists" => $exists]);
-            $this->logger->error("params", ["systemtagid" => $exists, "objectid" => $file->getId(), "objecttype" => "files"]);
+            }
 
             $query = $this->connection->getQueryBuilder();
             $params = [
@@ -43,10 +39,11 @@ trait TimelineWriteTags
                 'objectid' => $query->createNamedParameter($file->getId(), IQueryBuilder::PARAM_STR),
                 'objecttype' => $query->createNamedParameter('files', IQueryBuilder::PARAM_STR),
             ];
-            $result = $query->insert('systemtag_object_mapping')
+            $query->insert('systemtag_object_mapping')
                 ->values($params)->executeStatement();
 
-            $this->logger->error("inserted", [$tag, $result]);
+            $this->logger->info("Added $tag to file {$file->getId()}");
         }
     }
 }
+
