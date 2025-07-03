940138533fa2df78436f5ffc30430d946b69376a
Varun Patil <radialapps@gmail.com>
2025-07-03 14:41:38 -0700

    test: print console logs
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- e2e/login.ts
+++ e2e/login.ts
@@ -2,6 +2,19 @@ import { expect, type PlaywrightTestArgs } from '@playwright/test';
 
 export function login(route: string) {
   return async ({ page }: PlaywrightTestArgs) => {
+    page.on('console', (msg) => {
+      switch (msg.type()) {
+        case 'error':
+          console.error('js_console=' + msg.text());
+          break;
+        case 'warning':
+          console.warn('js_console=' + msg.text());
+          break;
+        default:
+          console.log('js_console=' + msg.text());
+      }
+    });
+
     await page.setViewportSize({ width: 800, height: 600 });
     await page.goto('http://localhost:8080/index.php/apps/memories' + route);
 
