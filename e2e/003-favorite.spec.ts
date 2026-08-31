ed0881cd22ab2dd24308deb3a6d6a8ff175f1862
Varun Patil <radialapps@gmail.com>
2026-08-31 16:01:29 -0700

    e2e: add favorite test
    Signed-off-by: Varun Patil <radialapps@gmail.com>

---
+++ e2e/003-favorite.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/test';
+import { appUrl, ocsHeaders, bootstrap } from './navigation';
+import { getFileId } from './utils';
+
+test.use({ extraHTTPHeaders: ocsHeaders });
+
+test.describe.serial.only('@ui Favorites', () => {
+  let fileid1: number;
+  let fileid2: number;
+
+  test.beforeAll(async ({ request }) => {
+    fileid1 = await getFileId(request, '/Photos/NKcupJh-Dos.jpg');
+    fileid2 = await getFileId(request, '/Photos/CbBbaNTmsAc.jpg');
+  });
+
+  test.beforeEach(async ({ page }) => {
+    await bootstrap(page);
+  });
+
+  test('Favorite file', async ({ page }) => {
+    await page.goto(appUrl);
+
+    await page.hover(`.p-outer--${fileid1}`);
+    await page.locator(`.p-outer--${fileid1} > div.select`).click();
+    await page.hover(`.p-outer--${fileid2}`);
+    await page.locator(`.p-outer--${fileid2} > div.select`).click();
+
+    await expect(page.locator(`.p-outer--${fileid1} .flag.bottom-right > .star-icon`)).not.toBeVisible();
+    await expect(page.locator(`.p-outer--${fileid2} .flag.bottom-right > .star-icon`)).not.toBeVisible();
+
+    await page.getByRole('button', { name: 'Actions' }).click();
+    await page.getByRole('menuitem', { name: 'Favorite' }).click();
+
+    await expect(page.locator(`.p-outer--${fileid1} .flag.bottom-right > .star-icon`)).toBeVisible();
+    await expect(page.locator(`.p-outer--${fileid2} .flag.bottom-right > .star-icon`)).toBeVisible();
+  });
+
+  test('Unfavorite file', async ({ page }) => {
+    await page.goto(appUrl);
+
+    await page.hover(`.p-outer--${fileid1}`);
+    await page.locator(`.p-outer--${fileid1} > div.select`).click();
+    await page.hover(`.p-outer--${fileid2}`);
+    await page.locator(`.p-outer--${fileid2} > div.select`).click();
+
+    await expect(page.locator(`.p-outer--${fileid1} .flag.bottom-right > .star-icon`)).toBeVisible();
+    await expect(page.locator(`.p-outer--${fileid2} .flag.bottom-right > .star-icon`)).toBeVisible();
+
+    await page.getByRole('button', { name: 'Actions' }).click();
+    await page.getByRole('menuitem', { name: 'Favorite' }).click();
+
+    await expect(page.locator(`.p-outer--${fileid1} .flag.bottom-right > .star-icon`)).not.toBeVisible();
+    await expect(page.locator(`.p-outer--${fileid2} .flag.bottom-right > .star-icon`)).not.toBeVisible();
+  });
+});
