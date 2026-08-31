7ee8d15b7ea058b0089c5371faaded89b79fc507
Varun Patil <radialapps@gmail.com>
2026-08-31 05:40:55 -0700

    e2e: add nested folders
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- e2e/002-timeline.spec.ts
+++ e2e/002-timeline.spec.ts
@@ -7,11 +7,13 @@ import assetPrimaryApiDay20696 from './assets/primary-api/day-20696.json';
 import assetPrimaryApiDay18962 from './assets/primary-api/day-18962.json';
 import assetPrimaryApiDay18955 from './assets/primary-api/day-18955.json';
 import assetPrimaryApiDay19221 from './assets/primary-api/day-19221.json';
+import assetPrimaryApiDay19468 from './assets/primary-api/day-19468.json';
 
 const TIMELINE_DAY_MAP: Record<any, IPhoto[]> = {
   '20696': assetPrimaryApiDay20696 satisfies IPhoto[],
   '18962': assetPrimaryApiDay18962 satisfies IPhoto[],
   '18955': assetPrimaryApiDay18955 satisfies IPhoto[],
+  '19468': assetPrimaryApiDay19468 satisfies IPhoto[],
   '19221': assetPrimaryApiDay19221 satisfies IPhoto[],
 };
 
@@ -46,15 +48,20 @@ test.describe('Timeline API', () => {
     });
     expect(res.ok()).toBeTruthy();
 
+    // Maximum of 5 days are preloaded (or 50 photos)
     const data: IDay[] = await res.json();
-    data.forEach((day) => day.detail?.forEach(cleanupPhoto));
+    for (let i = 0; i < 5; i++) {
+      const day = data[i];
+      expect(day.detail).toBeDefined();
+      expect(day.detail!.length).toBeGreaterThan(0);
+      day.detail!.forEach(cleanupPhoto);
+      if (day.dayid in TIMELINE_DAY_MAP) {
+        expect(day.detail).toStrictEqual(TIMELINE_DAY_MAP[day.dayid]);
+      }
+      delete day.detail;
+    }
 
-    expect(data).toStrictEqual(
-      (assetPrimaryApiDays satisfies IDay[]).map((day) => ({
-        ...day,
-        detail: TIMELINE_DAY_MAP[day.dayid] ?? undefined,
-      })),
-    );
+    expect(data).toStrictEqual(assetPrimaryApiDays satisfies IDay[]);
   });
 
   // Tests OCA\Memories\Controller\DaysController::day()
