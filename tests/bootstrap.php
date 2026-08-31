7bd1b0534a5cccb4e7d36c8b385eb242ba06152b
Varun Patil <radialapps@gmail.com>
2026-08-30 20:42:01 -0700

    ci: add unit workflow

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,17 @@
 
 declare(strict_types=1);
 
+use OCP\App\IAppManager;
+use OCP\Server;
+
 if (!defined('PHPUNIT_RUN')) {
     define('PHPUNIT_RUN', 1);
 }
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-if (file_exists(__DIR__.'/../../../lib/base.php')) {
-    require_once __DIR__.'/../../../lib/base.php';
-}
+require_once __DIR__.'/../../../lib/base.php';
+
+require_once __DIR__.'/../../../tests/autoload.php';
+
+Server::get(IAppManager::class)->loadApp('memories');
