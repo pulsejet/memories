19ff798f74ada3d8d5468651af89818bfa087bd1
Varun Patil <radialapps@gmail.com>
2026-08-31 14:38:09 -0700

    e2e: improve test
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- e2e/700-timeline.ui.spec.ts
+++ e2e/700-timeline.ui.spec.ts
@@ -1,20 +1,27 @@
 import { test, expect } from '@playwright/test';
 import { appUrl, bootstrap } from './navigation';
+import { getFileIdByBasename } from './utils';
 
-test.describe('@ui Timeline feed and photo preview', () => {
+test.describe.only('@ui Timeline feed and photo preview', () => {
+  let fileid1: number;
+
+  test.beforeAll(async ({ request }) => {
+    fileid1 = await getFileIdByBasename(request, 20696, 'NKcupJh-Dos.jpg');
+  });
+
   test.beforeEach(async ({ page }) => {
     await bootstrap(page);
-    await page.goto(appUrl);
-    await page.waitForSelector('.img-outer');
-    await page.waitForTimeout(500);
   });
 
   test('Look for Images', async ({ page }) => {
-    expect(await page.locator('.img-outer').count(), 'Number of previews').toBeGreaterThan(4);
+    await page.goto(appUrl);
+    await page.waitForSelector(`.p-outer--${fileid1}`);
+    expect(await page.locator('.p-outer').count()).toBeGreaterThan(4);
   });
 
   test('Open one image', async ({ page }) => {
-    await page.locator('.img-outer').first().click();
+    await page.goto(appUrl);
+    await page.locator(`.p-outer--${fileid1}`).click();
     await page.waitForTimeout(1000);
     await page.locator('button[title="Close"]').first().click();
   });
