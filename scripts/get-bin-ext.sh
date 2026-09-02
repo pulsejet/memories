8631735ea6f77655506048a2f2b5ccd08a7e555e
Varun Patil <radialapps@gmail.com>
2026-09-02 06:05:16 -0700

    bin-ext: parallelize download
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- scripts/get-bin-ext.sh
+++ scripts/get-bin-ext.sh
@@ -22,29 +22,40 @@ arch() {
 EXIFTOOL_VER=$(binExtVar EXIFTOOL_VER)
 GOVOD_VER=$(binExtVar GOVOD_VER)
 
-# Get exiftool prebuilt binaries
+# Clear up existing binaries.
 rm -rf bin-ext
 mkdir -p bin-ext
 cd bin-ext
+
+# Get prebuilt exiftool binaries with embedded perl.
 echo "Getting exiftool $EXIFTOOL_VER"
-wget -q "https://github.com/pulsejet/exiftool-bin/releases/download/$EXIFTOOL_VER/exiftool-amd64-musl"
-wget -q "https://github.com/pulsejet/exiftool-bin/releases/download/$EXIFTOOL_VER/exiftool-amd64-glibc"
-wget -q "https://github.com/pulsejet/exiftool-bin/releases/download/$EXIFTOOL_VER/exiftool-aarch64-musl"
-wget -q "https://github.com/pulsejet/exiftool-bin/releases/download/$EXIFTOOL_VER/exiftool-aarch64-glibc"
-chmod 755 *
+wget -q "https://github.com/pulsejet/exiftool-bin/releases/download/$EXIFTOOL_VER/exiftool-amd64-musl" & pid_et_amd64_musl=$!
+wget -q "https://github.com/pulsejet/exiftool-bin/releases/download/$EXIFTOOL_VER/exiftool-amd64-glibc" & pid_et_amd64_glibc=$!
+wget -q "https://github.com/pulsejet/exiftool-bin/releases/download/$EXIFTOOL_VER/exiftool-aarch64-musl" & pid_et_aarch64_musl=$!
+wget -q "https://github.com/pulsejet/exiftool-bin/releases/download/$EXIFTOOL_VER/exiftool-aarch64-glibc" & pid_et_aarch64_glibc=$!
 
-# Get exiftool source
-wget -q "https://github.com/exiftool/exiftool/archive/refs/tags/$EXIFTOOL_VER.zip"
-unzip -qq "$EXIFTOOL_VER.zip"
-mv "exiftool-$EXIFTOOL_VER" exiftool
-rm -rf *.zip exiftool/t exiftool/html exiftool/windows_exiftool
-chmod 755 exiftool/exiftool
+( # Get exiftool source for direct perl invocation.
+    wget -q "https://github.com/exiftool/exiftool/archive/refs/tags/$EXIFTOOL_VER.zip"
+    unzip -qq "$EXIFTOOL_VER.zip"
+    mv "exiftool-$EXIFTOOL_VER" exiftool
+    rm -rf "$EXIFTOOL_VER.zip" exiftool/t exiftool/html exiftool/windows_exiftool
+    chmod 755 exiftool/exiftool
+) & pid_et_src=$!
 
-# Get go-vod prebuilt binaries
+# Get prebuilt go-vod binaries.
 echo "Getting go-vod $GOVOD_VER"
-wget -q "https://github.com/pulsejet/memories/releases/download/go-vod/$GOVOD_VER/go-vod-amd64"
-wget -q "https://github.com/pulsejet/memories/releases/download/go-vod/$GOVOD_VER/go-vod-aarch64"
-chmod 755 go-vod-*
+wget -q "https://github.com/pulsejet/memories/releases/download/go-vod/$GOVOD_VER/go-vod-amd64" & pid_vod_amd64=$!
+wget -q "https://github.com/pulsejet/memories/releases/download/go-vod/$GOVOD_VER/go-vod-aarch64" & pid_vod_aarch64=$!
+
+wait "$pid_et_amd64_musl"
+wait "$pid_et_amd64_glibc"
+wait "$pid_et_aarch64_musl"
+wait "$pid_et_aarch64_glibc"
+wait "$pid_et_src"
+wait "$pid_vod_amd64"
+wait "$pid_vod_aarch64"
+
+chmod 755 exiftool/exiftool exiftool-* go-vod-*
 
 # Check the version of go-vod is correct internally
 if [ "$(./go-vod-$(arch) -version)" != "go-vod $GOVOD_VER" ]; then
