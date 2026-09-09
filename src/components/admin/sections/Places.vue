1d8f656a91780fb1fd8497d69319acf2d2328649
Varun Patil <radialapps@gmail.com>
2026-09-08 21:00:44 -0700

    fix(i18n): correct grammar in reverse geocoding string
    Use singular OpenStreetMap without article.
    
    Closes #1686.
    
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- src/components/admin/sections/Places.vue
+++ src/components/admin/sections/Places.vue
@@ -37,7 +37,7 @@
       {{
         t(
           'memories',
-          'Memories supports offline reverse geocoding using the OpenStreetMaps data on MySQL and Postgres.',
+          'Memories supports offline reverse geocoding using OpenStreetMap data on MySQL and Postgres.',
         )
       }}
       <br />
