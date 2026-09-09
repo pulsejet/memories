b560e26e28cd62943d7e8b54de5844e60e55917d
Varun Patil <radialapps@gmail.com>
2026-09-09 00:08:59 -0700

    android: set exportSchema=false

--- android/app/src/main/java/gallery/memories/dao/AppDatabase.kt
+++ android/app/src/main/java/gallery/memories/dao/AppDatabase.kt
@@ -9,7 +9,7 @@ import gallery.memories.R
 import gallery.memories.mapper.Photo
 
 
-@Database(entities = [Photo::class], version = 34)
+@Database(entities = [Photo::class], version = 34, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun photoDao(): PhotoDao
 
