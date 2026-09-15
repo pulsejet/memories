78fca92b5442fa16655616095237a6e3b70ee932
Varun Patil <radialapps@gmail.com>
2026-09-15 13:46:00 -0700

    bin-ext: bump go-vod
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- lib/Service/BinExt.php
+++ lib/Service/BinExt.php
@@ -10,7 +10,7 @@ use OCA\Memories\Util;
 final class BinExt
 {
     public const EXIFTOOL_VER = '13.59';
-    public const GOVOD_VER = '0.3.0';
+    public const GOVOD_VER = '0.4.0';
     public const NX_VER_MIN = '1.1';
 
     /** Exiftool environment is initialized in this process */
