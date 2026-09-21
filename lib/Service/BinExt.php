345358e4b7c90e06571c5a0a3d07a870526e89da
Varun Patil <radialapps@gmail.com>
2026-09-21 13:12:16 -0700

    chore(go-vod): bump BinExt GOVOD_VER to 0.5.0
    Signed-off-by: Varun Patil <radialapps@gmail.com>

--- lib/Service/BinExt.php
+++ lib/Service/BinExt.php
@@ -10,7 +10,7 @@ use OCA\Memories\Util;
 final class BinExt
 {
     public const EXIFTOOL_VER = '13.59';
-    public const GOVOD_VER = '0.4.2';
+    public const GOVOD_VER = '0.5.0';
     public const NX_VER_MIN = '1.1';
 
     private const GO_VOD_PID_FILE = '/tmp/go-vod.pid';
